697743ca7f901e8a8ebea47fbbbada274d05fe08
Bishop Shaw III <prototype124@yahoo.com>
2023-09-02 18:22:36 -0700

    added routes to app.py

--- app.py
+++ app.py
@@ -22,6 +22,16 @@ with app.app_context():
 
 @app.route("/")
 def index():
+    return render_template("index.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+@app.route("/login")
+def login():
     return render_template("login.html")
 
 
@@ -33,6 +43,9 @@ def dashboard():
 @app.route("/new_note")
 def new_route():
     return render_template("notes.html")
+
+# @app.route("/logout")
+# def logout():
 
 
 if __name__ == '__main__':
